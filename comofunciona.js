5fea29bf9028621a9380b3618d9b2cb9ce7dd20e
Sophia-Coelho <162128128+Sophia-Coelho@users.noreply.github.com>
2025-11-17 18:47:59 -0300

    Criei uma pasta JS e CSS

--- comofunciona.js
+++
@@ -1,7 +0,0 @@
-
-const botaoComecar = document.getElementById('btn-comecar');
-
-botaoComecar.addEventListener('click', function() {
-    console.log("Iniciando redirecionamento para o cadastro...");
-    window.location.href = "cadastro.html"; 
-});
