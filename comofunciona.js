46ada3e6b81b44524c86057e028dc5d48c6f7cad
GabrielSiSo <gabrieldasilvasoares2005@gmail.com>
2025-11-17 18:45:22 -0300

    como funciona

---
+++ comofunciona.js
@@ -0,0 +1,7 @@
+
+const botaoComecar = document.getElementById('btn-comecar');
+
+botaoComecar.addEventListener('click', function() {
+    console.log("Iniciando redirecionamento para o cadastro...");
+    window.location.href = "cadastro.html"; 
+});
