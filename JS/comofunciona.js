ca62f5e4f77d35375adfbecaa4b72f7267dc416c
nalugao <nalugao@hotmail.com>
2025-11-21 19:42:32 -0300

    ajuste botão na pg COMO FUNCIONA

--- JS/comofunciona.js
+++ JS/comofunciona.js
@@ -3,5 +3,5 @@ const botaoComecar = document.getElementById('btn-comecar');
 
 botaoComecar.addEventListener('click', function() {
     console.log("Iniciando redirecionamento para o cadastro...");
-    window.location.href = "cadastro.html"; 
+    window.location.href = "Cadastro.html"; 
 });
